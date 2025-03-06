002d74e99da8cc8dad1fd3888055c5b602162c9a
AdelinGabriel21 <116873929+AdelinGabriel21@users.noreply.github.com>
2025-03-06 19:57:44 +0200

    class dog added

--- main.cpp
+++ main.cpp
@@ -1,7 +1,33 @@
+#include <iostream>
 //
 // Created by adeli on 3/6/2025.
 //
 
+
+class Dog {
+
+public:
+  bool eat() {
+    std::cout<<"Dog is eating";
+    return true;
+  }
+  int setage(int newage) {
+    age = newage;
+    return age;
+  }
+  int getage() {
+    return age;
+  }
+private:
+  int age;
+};
+
+
 int main(){
+  Dog dog1 = Dog();
+  dog1.eat();
+  // dog1.age = 50;
+  dog1.setage(10);
+  std::cout << dog1.getage();
   return 0;
 }
